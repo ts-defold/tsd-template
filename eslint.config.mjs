c0b9a34d135b4ba7044e3a7aa33201d1a81d640e
thinknathan <github@thinknathan.com>
2024-04-12 18:06:44 -0700

    Add prettier, update eslint to v9

---
+++ eslint.config.mjs
@@ -0,0 +1,57 @@
+// @ts-check
+
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+	eslint.configs.recommended,
+	...tseslint.configs.recommendedTypeChecked,
+	{
+		plugins: {
+			'@typescript-eslint': tseslint.plugin,
+		},
+		languageOptions: {
+			parser: tseslint.parser,
+			parserOptions: {
+				project: true,
+			},
+		},
+		rules: {
+			eqeqeq: 'error',
+			'@typescript-eslint/ban-types': [
+				'error',
+				{
+					types: {
+						null: {
+							message: "Use `undefined` as the equivalent to Lua's `nil`",
+							fixWith: 'undefined',
+							suggest: ['undefined'],
+						},
+					},
+					extendDefaults: true,
+				},
+			],
+			'no-unused-vars': 'off',
+			'@typescript-eslint/no-unused-vars': [
+				'error',
+				{
+					args: 'all',
+					argsIgnorePattern: '^_',
+					caughtErrors: 'all',
+					caughtErrorsIgnorePattern: '^_',
+					destructuredArrayIgnorePattern: '^_',
+					varsIgnorePattern: '^_',
+					ignoreRestSiblings: true,
+				},
+			],
+			'@typescript-eslint/strict-boolean-expressions': [
+				'error',
+				{
+					allowString: false,
+					allowNumber: false,
+				},
+			],
+			'@typescript-eslint/restrict-plus-operands': 'off',
+		},
+	},
+);
